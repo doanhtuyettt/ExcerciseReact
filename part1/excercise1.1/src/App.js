e91873b46b6d972f7a4191f269ca4e71ba74d1d0
Tuyet <doanhtuyet_t64@hus.edu.vn>
2022-07-14 16:53:25 +0700

    init

---
+++ part1/excercise1.1/src/App.js
@@ -0,0 +1,66 @@
+
+const App = () => {
+  const course = 'Half Stack application development'
+  
+
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts =[{
+    part: part1,
+    excercise : exercises1
+  }
+  ,{
+    part: part2,
+    excercise : exercises2
+  }
+  ,{
+    part: part3,
+    excercise : exercises3
+  }
+  ]
+  return (
+    <>
+      <Header course ={course}/>
+      <Content parts = {parts}/>
+      <Total parts = {parts}/>
+    </>
+  )
+}
+
+const Header =(props) =>{
+  return(
+    <>
+    <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content =(props) =>{
+  return (
+    <>
+      <Part parts={props.parts[0]}/>
+      <Part parts={props.parts[1]}/>
+      <Part parts={props.parts[2]}/>
+    </>
+  )
+}
+
+const Part =(props) =>{
+  return(
+    <p> {props.parts.part} {props.parts.excercise}</p>
+    )
+
+}
+const Total= (props) =>{
+  return(
+    <>
+      <p>Number of exercises {props.parts[0].excercise + props.parts[1].excercise + props.parts[2].excercise }</p>
+    </>
+  )
+}
+
+export default App
